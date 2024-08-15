a05322ac9f1ab4ed70354c9dc296f65ae6e6f6d4
tsukipond8531 <reader2027@gmail.com>
2024-08-15 06:45:13 -0700

    Update Navbar

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 
+import './index.css';
+
 export default function Navbar () {
     return (
-        <nav className="navbar sticky-top navbar-expand-lg bg-light">
-            <div className="container">
-                <a className="navbar-brand" href="/">
+        <nav className="navbar sticky-top navbar-expand-lg bg-white border-bottom">
+            <div className="container d-flex">
+                <a className="navbar-brand order-1 order-lg-0" href="/">
                     <img src="/assets/logos/logo_225_color.png" alt="EcoBots logo" style={{width: 70, height: 70}} />
                 </a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                <div className="d-flex order-2 order-lg-2">
+                    <a className="btn btn-outline-success fw-semibold" href="/chat/">Try it now</a>
+                </div>
+                <button className="navbar-toggler order-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon me-1"></span>Menu
                 </button>
-                <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                <div className="offcanvas offcanvas-top flex-row navbar-collapse order-lg-1" id="navbarToggler">
+                    <div className="offcanvas-body">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
                             <a className="nav-link" aria-current="page" href="/">Home</a>
@@ -19,9 +25,7 @@ export default function Navbar () {
                             <a className="nav-link" href="/about/">About us</a>
                         </li>
                     </ul>
-                </div>
-                <div className="d-flex">
-                    <a className="btn btn-outline-success" href="/chat/">Try it now</a>
+                    </div>
                 </div>
             </div>
         </nav>
