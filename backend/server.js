88c72f2b3360364f83bff37403ccce9084bc7f8c
tsukipond8531 <reader2027@gmail.com>
2024-08-15 19:44:52 -0700

    Create backend

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+// Define routes and middleware
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
