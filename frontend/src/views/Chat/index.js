1dabea2ebece5b15fc464f01ffe8f3ad10c4433e
tsukipond8531 <reader2027@gmail.com>
2024-08-16 10:11:39 -0700

    Add ModeSelector

--- frontend/src/views/Chat/index.js
+++ frontend/src/views/Chat/index.js
@@ -1,8 +1,50 @@
 import React from 'react';
 
+import ChatButton from 'components/ChatButton';
+import StopGenerating from './stopgenerating';
+import ModeSelector from './modeselector';
+
+import './index.css';
+
 export default function Chat () {
+    function new_conversation () {
+        console.log("a");
+    }
+
+    function new_conversation_guided () {
+
+    }
+
     return (
-        <>
-        </>
+        <div className="container-fluid">
+            <div className="row chat-container">
+                <div className="col col-12 col-md-3 d-md-block d-none sidebar-container">
+                    <div className="sidebar-top">
+                        <div className="chat-tool-container">
+                            <ChatButton title="New Empty Conversation" icon="fa-plus-circle" onClick={new_conversation} />
+                            <ChatButton title="New Guided Conversation" icon="fa-plus-hexagon" onClick={new_conversation_guided} />
+                        </div>
+                        <div className="spinner"></div>
+                    </div>
+                </div>
+                <div className="col col-12 col-md-9 chat-box-container">
+                    <div className="h-100">
+                        <StopGenerating />
+                        <ModeSelector />
+                        <div className="chat-box">
+                            <img src="/assets/logos/logo_1024.png" id="placeholder_img"/>
+                        </div>
+                        <div className="user-input h-auto">
+                            <div className="box input-box">
+                                <textarea id="message-input" placeholder="Ask a question" cols="30" rows="10"></textarea>
+                                <div id="send-button">
+                                    <i className="fa fa-paper-plane-top fa-regular"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
---
+++ frontend/src/views/Chat/modeselector.js
@@ -0,0 +1,72 @@
+import React, { useRef, useEffect, useContext } from 'react';
+
+import UserContext from 'hooks/contexts/usercontext';
+import { CHATMODE_ECOBOTS, CHATMODE_GUIDED, CHATMODE_STANDARD } from 'hooks/contexts/usercontext';
+
+export default function ModeSelector () {
+
+    const { chatMode, setChatMode } = useContext(UserContext);
+
+    const modeEcobotsRef = useRef();
+    const modeGuidedRef = useRef();
+    const modeStandardRef = useRef();
+
+    const handleChatMode = (mode, ref) => {
+        setChatMode(mode);
+        ref.current.focus();
+    }
+
+    useEffect(() => {
+        new window.bootstrap.Popover(modeEcobotsRef.current, {
+            title: "Our most accurate model teaching you about local and global ecology.",
+            content: "Available to all users for free.",
+            trigger: "focus",
+            placement: "bottom",
+            customClass: "custom-popover",
+        })
+        
+        new window.bootstrap.Popover(modeGuidedRef.current, {
+            title: "Use our Standard model will ask you questions about ecology and sustainablility.",
+            content: "Available to all users for free.",
+            trigger: "focus",
+            placement: "bottom",
+            customClass: "custom-popover",
+        })
+        
+        new window.bootstrap.Popover(modeStandardRef.current, {
+            title: "Standard model assisting you with everyday inquiries.",
+            content: "Available to all users for free.",
+            trigger: "focus",
+            placement: "bottom",
+            customClass: "custom-popover",
+        })
+    }, []);
+
+    return (
+        <div className="mode-selector-container">
+            <div className="mode-selector">
+                <button 
+                    className={`btn btn-success mode-button ${chatMode === CHATMODE_ECOBOTS ? "active" : ""}`} 
+                    ref={modeEcobotsRef}
+                    onClick={() => handleChatMode(CHATMODE_ECOBOTS, modeEcobotsRef)}
+                >
+                    EcoBots
+                </button>
+                <button 
+                    className={`btn btn-success mode-button ${chatMode === CHATMODE_GUIDED ? "active" : ""}`} 
+                    ref={modeGuidedRef}
+                    onClick={() => handleChatMode(CHATMODE_GUIDED, modeGuidedRef)}
+                >
+                    Guided
+                </button>
+                <button 
+                    className={`btn btn-success mode-button ${chatMode === CHATMODE_STANDARD ? "active" : ""}`} 
+                    ref={modeStandardRef} 
+                    onClick={() => handleChatMode(CHATMODE_STANDARD, modeStandardRef)}
+                >
+                    Standard
+                </button>
+            </div>
+        </div>
+    )
+}
