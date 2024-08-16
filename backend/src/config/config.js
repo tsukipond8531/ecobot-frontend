36f86b74c346efe78cc4e3a5ce589443cbb1926e
tsukipond8531 <reader2027@gmail.com>
2024-08-16 02:02:34 -0700

    Add config

---
+++ backend/src/config/config.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default {
+    NODE_ENV: process.env.NODE_ENV || 'DEV',
+    HOST: process.env.HOST || '0.0.0.0',
+    PORT: process.env.PORT || '8080',
+    expiresIn: process.env.EXPIRESIN || '24h',
+    SENTRY_DSN: process.env.SENTRY_DSN || '',
+  }
+  
