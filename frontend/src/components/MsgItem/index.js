cbbf758f6c977c4426e3e7c4ab50ee0042f947ab
tsukipond8531 <reader2027@gmail.com>
2024-08-19 00:59:55 -0700

    Update message box

--- frontend/src/components/MsgItem/index.js
+++ frontend/src/components/MsgItem/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
+import './index.css';
+
+const user_image = "/assets/chats/user.png";
+const gpt_image = "/assets/chats/ecobot.png";
+
+const md = window.markdownit();
+
 export default function MsgItem (props) {
+    const { message } = props;
+
+    const image = message.role === "user" ? user_image : gpt_image;
+
     return (
-        <>
-        </>
+        <div className={`message-item ${message.display === "false" ? "d-none" : ""} ${message.role === "assistant" ? "ecobot" : ""}`}>
+            <div className="role">
+                <img src={image} alt="Avatar"></img>
+            </div>
+            <div className="content" dangerouslySetInnerHTML={{ __html: md.render(message.content) }} >
+            </div>
+        </div>
     )
 }
--- frontend/src/views/Chat/messagebox.js
+++ frontend/src/views/Chat/messagebox.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
+import MsgItem from 'components/MsgItem';
+
 export default function MessageBox (props) {
+    const { items, talks } = props;
+
     return (
         <div className="chat-box">
             <img src="/assets/logos/logo_1024.png" alt="Background" id="placeholder_img"/>
+            {items?.length > 0 && items.map((it, idx) => (
+                <MsgItem message={it} key={idx} />
+            ))}
+            {talks?.length > 0 && talks.map((it, idx) => (
+                <MsgItem message={it} key={idx} />
+            ))}
         </div>
     )
 }
