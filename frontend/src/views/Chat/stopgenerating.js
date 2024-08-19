c8e66d51d6bc55b880d070e6a967cbdb0328dcba
tsukipond8531 <reader2027@gmail.com>
2024-08-19 01:07:27 -0700

    add chat state to stopgenerating

--- frontend/src/views/Chat/stopgenerating.js
+++ frontend/src/views/Chat/stopgenerating.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import UserContext from 'hooks/contexts/usercontext';
+import { CHATSTATE_GENERATING } from 'hooks/contexts/usercontext';
 
 export default function StopGenerating () {
+    const { chatState } = useContext(UserContext);
+
     return (
-        <div className="stop-generating">
+        <div className={`stop-generating ${chatState !== CHATSTATE_GENERATING ? "d-none" : ""}`}>
             <button id="cancelButton" className="border-0 m-auto fw-bold">
                 <span>Stop Generating</span>
                 <i className="fa-regular fa-stop ms-2 fw-bold"></i>
