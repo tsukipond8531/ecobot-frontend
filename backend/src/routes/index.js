ce52fae29b3f10844f811a40e44eb343fc62bae2
tsukipond8531 <reader2027@gmail.com>
2024-08-20 05:52:51 -0700

    add auth middleware
    add device_uuid to req body

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 
-import { fetchUUID, fetchGptMessage } from '../controller';
+import auth from '../middlewares/auth'
+
+import { fetchUUID, fetchGptMessage, saveMessage, setMessageRate } from '../controller';
 
 const router = Router();
 
 router.get('/uuid', fetchUUID);
-router.get('/gpt_message', fetchGptMessage);
+router.post('/gpt_message', auth(), fetchGptMessage);
+router.post('/save_message', auth(), saveMessage)
+router.post('/set_message_rate', auth(), setMessageRate);
 
 export default router;
