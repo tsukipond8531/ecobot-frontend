4016060be7c307ce51dcf0c4e8a7a58e72e6d37d
tsukipond8531 <reader2027@gmail.com>
2024-08-22 23:38:50 -0700

    add components

---
+++ ecobots/src/frontend/src/components/MsgItem/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import MarkdownIt from 'markdown-it';
+import './index.css';
+
+const user_image = "/assets/chats/user.png";
+const gpt_image = "/assets/chats/ecobot.png";
+
+const md = new MarkdownIt();
+
+interface Message {
+    role: "user" | "assistant";
+    content: string;
+    display: "true" | "false" | "loading";
+}
+
+interface MsgItemProps {
+    message: Message;
+}
+
+export default function MsgItem({ message }: MsgItemProps) {
+    const image = message.role === "user" ? user_image : gpt_image;
+
+    return (
+        <div className={`message-item ${message.role === "assistant" ? "ecobot" : ""} ${message.display === "false" ? "d-none" : ""}`}>
+            <div className="role">
+                <img src={image} alt="Avatar"></img>
+            </div>
+            <div className={`content ${message.display === "loading" ? "d-none" : ""}`} dangerouslySetInnerHTML={{ __html: md.render(message.content) }} />
+            <div className="content">
+                {message.display === "loading" && <div className="loading"></div>}
+            </div>
+        </div>
+    );
+}
